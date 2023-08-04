26134b73f85c9a393161c37308cef8c6bf25f48d
kai.guo <gkmails@126.com>
2023-08-04 19:27:40 +0800

    模拟仿真平台发送数据

--- Example3/Echo.cs
+++ Example3/Echo.cs
@@ -1,19 +1,42 @@
 using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading;
 using WebSocketSharp;
 using WebSocketSharp.Server;
 
 namespace Example3
 {
-  public class Echo : WebSocketBehavior
-  {
-    protected override void OnMessage (MessageEventArgs e)
+    
+    public class Test
     {
-      Send (e.Data + "-server");
+        public List<object> series{ get; set; }
     }
+    public class Echo : WebSocketBehavior
+    {
 
+        private int i = 10;
+        System.Threading.Timer timer;
+        protected override void OnMessage (MessageEventArgs e)
+        {
+            if (e.Data == "start")
+            {
+                timer = new System.Threading.Timer(new TimerCallback(SendData), this, 0, 1000);//创建定时器
+            }
+        }
+
+        private void SendData(object obj)
+        {
+            var t = new Test();
+            Random ran = new Random();
+            t.series = new List<object> { new { data = new List<int> { ran.Next(100, 1000), ran.Next(100, 1000), ran.Next(100, 1000), ran.Next(100, 1000), ran.Next(100, 1000), ran.Next(100, 1000) } } };
+            var str_data = JsonSerializer.Serialize(t);
+            Send(str_data);
+        }
         protected override void OnOpen()
         {
             Send("connect success!!!");
         }
     }
+
 }
